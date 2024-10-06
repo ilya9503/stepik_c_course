21562ed5dbfc415addfd04d7a9a5a785d9beddb5
Ilya Savchenko <sav306@yandex.ru>
2024-10-07 00:24:37 +0300

    feat: Добавить задачу по Fibonacci

--- chapter_4/Операторы циклов/test_3.c
+++ chapter_4/Операторы циклов/test_3.c
@@ -2,17 +2,20 @@
 
 int main(void)
 {
-    int n;
+    int n, sum = 0;
 
     if (scanf("%d", &n) != 1) {
         printf("Error input");
         return 0;
     }
 
-    while ((n / 10) != 0 && 1) {
-        
+    while (n > 0) {
+        int rem;        // остаток
+        rem = n % 10;
+        n = n / 10;
+        sum += rem; 
     }
-    
+    printf("%d", sum);
 
     return 0;
 }
